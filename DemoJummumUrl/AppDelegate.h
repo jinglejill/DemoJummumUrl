b0b2d4c542ee7823077e447452cc603da3f28292
Thidaporn Kijkamjai <jinglejill@me.com>
2018-08-01 05:31:28 +0700

    Initial Commit

---
+++ DemoJummumUrl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoJummumUrl
+//
+//  Created by Thidaporn Kijkamjai on 1/8/2561 BE.
+//  Copyright © 2561 Jummum Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
